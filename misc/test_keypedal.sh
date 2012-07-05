b356eaf4f24633e4a1dfdcacd6d1dcb6a9018df6
Mark Van den Borre <mark@markvdb.be>
2012-07-05 22:30:47 +0200

    update key pedal test script, document camera settings

--- misc/test_keypedal.sh
+++ misc/test_keypedal.sh
@@ -1,46 +1,67 @@
 #! /bin/bash
 
+LANG=C
 TMOUT=10
-PTPCAM='/usr/src/ptpcam/ptpcam'
+PTPCAM='/usr/bin/ptpcam'
 
-GPHOTOCAM1=$(gphoto2 --auto-detect|grep usb|sed -e 's/.*Camera *//g'|head -n1)
-GPHOTOCAM2=$(gphoto2 --auto-detect|grep usb|sed -e 's/.*Camera *//g'|tail -n1)
+function detect_cams {
+  CAMS=$(gphoto2 --auto-detect|grep usb| wc -l)
+  if [ $CAMS -eq 2 ]; then
+    GPHOTOCAM1=$(gphoto2 --auto-detect|grep usb|sed -e 's/.*Camera *//g'|head -n1)
+    GPHOTOCAM2=$(gphoto2 --auto-detect|grep usb|sed -e 's/.*Camera *//g'|tail -n1)
+    CAM1=$(echo $GPHOTOCAM1|sed -e 's/.*,//g')
+    CAM2=$(echo $GPHOTOCAM2|sed -e 's/.*,//g')
+    echo "Detected 2 camera devices: $GPHOTOCAM1 and $GPHOTOCAM2"
+  else
+    echo "Number of camera devices does not equal 2. Giving up."
+    exit
+  fi
+}
 
-CAM1=$(echo GPHOTOCAM1|sed -e 's/.*,//g')
-CAM2=$(echo GPHOTOCAM2|sed -e 's/.*,//g')
+function delete_from_cams {
+  echo "$GPHOTOCAM1: deleting existing images from SD card"
+  gphoto2 --port $GPHOTOCAM1 --recurse -D A/store00010001/DCIM/; true
+  echo "$GPHOTOCAM2: deleting existing images from SD card"
+  gphoto2 --port $GPHOTOCAM2 --recurse -D A/store00010001/DCIM/; true
+}
 
-echo "Found cameras: $GPHOTOCAM1 $GPHOTOCAM2"
+function switch_to_record_mode {
+  echo "Switching cameras to record mode..."
+  echo "$CAM1 is CAM1 and $CAM2 is CAM2"
+  $PTPCAM --dev=$CAM1 --chdk='mode 1' > /dev/null 2>&1
+  $PTPCAM --dev=$CAM2 --chdk='mode 1' > /dev/null 2>&1
+}
 
-echo "$GPHOTOCAM1: deleting existing images from SD card"
-gphoto2 --port $GPHOTOCAM1 --recurse -D A/store00010001/DCIM/
-echo "$GPHOTOCAM2: deleting existing images from SD card"
-gphoto2 --port $GPHOTOCAM2 --recurse -D A/store00010001/DCIM/
+function download_from_cams {
+    echo "Downloading images from $CAM1..."
+    # gphoto2 processes end with -1 unexpected result even though everything seems to be fine -> hack: true gives exit status 0
+    gphoto2 --port $GPHOTOCAM1 -P A/store_00010001/DCIM/; true
+    echo "Downloading images from $CAM2"
+    gphoto2 --port $GPHOTOCAM2 -P A/store_00010001/DCIM/; true 
+}
 
-echo "Switching cameras to record mode..."
-$PTPCAM --dev=$CAM1 --chdk='mode 1'
-$PTPCAM --dev=$CAM2 --chdk='mode 1'
+detect_cams
+# delete_from cams confuses ptpcam -> do that at the end
+switch_to_record_mode
 
 echo "Starting foot pedal loop..."
 while true
 do
   read -n1 shoot
-  if [ -z "$shoot" ]; then
-    echo "Foot pedal not pressed for $TMOUT seconds. Downloading images and deleting from cameras..."
-    echo "Downloading images from $CAM1"
-    echo "TODO: gphoto2 processes end with -1 unexpected result even though everything seems to be fine -> && part doesn't run"
-    gphoto2 --port $GPHOTOCAM1 -P A/store_00010001/DCIM/100___01gphoto2 --port $GPHOTOCAM1 --recurse -D A/store00010001/DCIM/
-    RETVAL = $?
-    [ $RETVAL -eq -1 ] && gphoto2 --port $GPHOTOCAM1 --recurse -D A/store00010001/DCIM/
-    echo "Downloading images from $CAM2"
-    gphoto2 --port $GPHOTOCAM2 -P A/store_00010001/DCIM/100___01
-    RETVAL = $?
-    [ $RETVAL -eq -1 ] && gphoto2 --port $GPHOTOCAM2 --recurse -D A/store00010001/DCIM/
-  else
+  if [ "$shoot" == "s" ]; then
     echo "Key pressed."
-    echo "Shooting with camera $CAM1"
+    echo "Shooting with cameras $CAM1 and $CAM2"
     $PTPCAM --dev=$CAM1 --chdk='lua shoot()'
-    echo "Shooting with camera $CAM2"
     $PTPCAM --dev=$CAM2 --chdk='lua shoot()'
+  elif [ "$shoot" == "q" ]; then
+    echo "Quitting without downloading images from cameras!"
+    exit
+  elif [ -z "$shoot" ]; then
+    echo "Foot pedal not pressed for $TMOUT seconds."
+    download_from_cams
+    delete_from_cams
+    echo "Quitting"
+    exit 
   fi
 done
 
