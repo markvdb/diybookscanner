1142769bcabe82b3ffe67c555444be4b943da95d
Mark Van den Borre <mark@markvdb.be>
2012-07-05 13:00:12 +0200

    add test with key pedal

---
+++ misc/test_keypedal.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+TMOUT=10
+PTPCAM='/usr/src/ptpcam/ptpcam'
+
+GPHOTOCAM1=$(gphoto2 --auto-detect|grep usb|sed -e 's/.*Camera *//g'|head -n1)
+GPHOTOCAM2=$(gphoto2 --auto-detect|grep usb|sed -e 's/.*Camera *//g'|tail -n1)
+
+CAM1=$(echo GPHOTOCAM1|sed -e 's/.*,//g')
+CAM2=$(echo GPHOTOCAM2|sed -e 's/.*,//g')
+
+echo "Found cameras: $GPHOTOCAM1 $GPHOTOCAM2"
+
+echo "$GPHOTOCAM1: deleting existing images from SD card"
+gphoto2 --port $GPHOTOCAM1 --recurse -D A/store00010001/DCIM/
+echo "$GPHOTOCAM2: deleting existing images from SD card"
+gphoto2 --port $GPHOTOCAM2 --recurse -D A/store00010001/DCIM/
+
+echo "Switching cameras to record mode..."
+$PTPCAM --dev=$CAM1 --chdk='mode 1'
+$PTPCAM --dev=$CAM2 --chdk='mode 1'
+
+echo "Starting foot pedal loop..."
+while true
+do
+  read -n1 shoot
+  if [ -z "$shoot" ]; then
+    echo "Foot pedal not pressed for $TMOUT seconds. Downloading images and deleting from cameras..."
+    echo "Downloading images from $CAM1"
+    echo "TODO: gphoto2 processes end with -1 unexpected result even though everything seems to be fine -> && part doesn't run"
+    gphoto2 --port $GPHOTOCAM1 -P A/store_00010001/DCIM/100___01gphoto2 --port $GPHOTOCAM1 --recurse -D A/store00010001/DCIM/
+    RETVAL = $?
+    [ $RETVAL -eq -1 ] && gphoto2 --port $GPHOTOCAM1 --recurse -D A/store00010001/DCIM/
+    echo "Downloading images from $CAM2"
+    gphoto2 --port $GPHOTOCAM2 -P A/store_00010001/DCIM/100___01
+    RETVAL = $?
+    [ $RETVAL -eq -1 ] && gphoto2 --port $GPHOTOCAM2 --recurse -D A/store00010001/DCIM/
+  else
+    echo "Key pressed."
+    echo "Shooting with camera $CAM1"
+    $PTPCAM --dev=$CAM1 --chdk='lua shoot()'
+    echo "Shooting with camera $CAM2"
+    $PTPCAM --dev=$CAM2 --chdk='lua shoot()'
+  fi
+done
+
+
---
+++ terminology.md
@@ -0,0 +1,11 @@
+diybookscanner
+==============
+
+unofficial diybookscanner repository (while waiting for approval by Dan &amp; Rob)
+
+TODO
+----
+* port every wooden part from DIY_Book_Scanner.9.2.0.dxf to an independent, 3D layered openscad file
+* recreate a sheet with all wooden parts from .9.2.0
+* model essential other parts (glass, L brackets, felt pads, ...)
+* create an assembled model of .9.2.0
